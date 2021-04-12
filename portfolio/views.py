1ab59c1a811f34ac9c74026c735ffa53cf551a75
Zachary Baca <zacharybaca@Zacharys-MacBook-Air.local>
2021-04-12 08:22:08 -0500

    started on index view

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+def index(request):
+    projects = Project.objects.all()
+    return render(request, "index.html", {"projects": projects})
--- portfoliodjango/urls.py
+++ portfoliodjango/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from portfolio.views import index
 
 urlpatterns = [
+    path('', index, name="index"),
     path('admin/', admin.site.urls),
 ]
 
