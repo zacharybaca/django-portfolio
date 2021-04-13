67e65e17a11923d42d7de561ab22fdf0861b9707
Zachary Baca <zacharybaca@Zacharys-MacBook-Air.local>
2021-04-13 05:48:26 -0500

    added blog model

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
 def all_blogs(request):
-    return render(request, 'all_blogs.html')
+    blogs = Blog.objects.order_by('-date')[:5]
+    return render(request, 'all_blogs.html', {"blogs": blogs})
