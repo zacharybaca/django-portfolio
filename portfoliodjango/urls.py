cd16e10d62d2ae77f42bf3a694d598e800192f8c
Zachary Baca <zacharybaca@Zacharys-MacBook-Air.local>
2021-04-13 03:18:52 -0500

    added blog view

--- portfoliodjango/urls.py
+++ portfoliodjango/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from portfolio.views import index
@@ -22,6 +22,7 @@ from portfolio.views import index
 urlpatterns = [
     path('', index, name="index"),
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
