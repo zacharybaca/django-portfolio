97f1bcdcc1075e78ea914e8e73b3563121aca673
Zachary Baca <zacharybaca@Zacharys-MacBook-Air.local>
2021-04-16 10:54:50 -0500

    added some useful functionality

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Blog
 # Create your views here.
 def all_blogs(request):
-    blogs = Blog.objects.order_by('-date')[:5]
+    blogs = Blog.objects.order_by('-date')
     return render(request, 'all_blogs.html', {"blogs": blogs})
 
 def detail(request, blog_id):
--- portfolio/models.py
+++ portfolio/models.py
@@ -6,3 +6,5 @@ class Project(models.Model):
     image = models.ImageField(upload_to='images/')
     url = models.URLField(blank=True)
 
+    def __str__(self):
+        return self.title
