000623b56a1401823334ca4e200013f7023500eb
zacharybaca <bacazach@icloud.com>
2021-04-28 16:20:07 +0000

    removed old files

--- portfoliodjango/settings.py
+++ portfoliodjango/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET', 'django-insecure-%_2(4)g5zsmu90!z139nozugn)9=!(l3@ku8p_ugg20irsp(ap')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zacharybaca.pythonanywhere.com']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
